560ac4e0e1f64c2d86a547df093861b0405f61e9
Amit Patange <amitpatange88@gmail.com>
2018-11-24 19:52:02 +0530

    comments logged

--- ReshimgathiMatrimony/Controllers/LoginController.cs
+++ ReshimgathiMatrimony/Controllers/LoginController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 
 namespace ReshimgathiMatrimony.Controllers
 {
+    /// <summary>
+    /// LoginController
+    /// </summary>
     public class LoginController : Controller
     {
         public ActionResult Index()
